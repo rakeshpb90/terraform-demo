70fc9afad199b00af25cd63477771cf913f238aa
rakeshpb90 <pb.rakesh90@gmail.com>
2023-11-24 22:48:12 +0530

    Test | test

--- templates/scripts/git_post.sh
+++ templates/scripts/git_post.sh
@@ -1,10 +1,16 @@
-#!/bin/bash -x 
+#!/bin/bash -x
 # Accept Command Line Arguments
 GITHUB_TOKEN=$1
 PR_NUMBER=$2
-COMMENT=$(cat terraform_plan_output.json)
+
+# Read the content of the text file
+COMMENT=$(<terraform_plan_output.txt)
+
+COMMENT=$(echo "$COMMENT" | jq -s -R '.' | sed 's/^"\(.*\)"$/\1/')
+
 curl -X POST \
   -H "Authorization: token $GITHUB_TOKEN" \
   -H "Content-Type: application/json" \
-  -d "{\"body\": \"$COMMENT\"}" \
-  "https://api.github.com/repos/rakeshpb90/terraform-demo/issues/$PR_NUMBER/comments"
+  -d '{"body": "'"$COMMENT"'"}' \
+  "https://api.github.com/repos/rakeshpb90/terraform-demo/issues/$PR_NUMBER/comments" \
+  -sS
